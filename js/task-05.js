abfa948ba28c79d8de1ba7262391e2e485532021
Zelenro <125198729+Zelenro@users.noreply.github.com>
2023-06-21 10:30:22 +0300

    1

--- js/task-05.js
+++
@@ -1,7 +0,0 @@
-const nameInputEl = document.querySelector('#name-input');
-const nameOutputEl = document.querySelector('#name-output');
-
-nameInputEl.addEventListener('input', event => {
-  const inputValue = event.currentTarget.value.trim();
-  nameOutputEl.textContent = inputValue !== '' ? inputValue : 'Anonymous';
-});
--- js/task-07.js
+++
@@ -1,5 +0,0 @@
-const fontSizeEl = document.querySelector('#font-size-control');
-const textEl = document.querySelector('#text');
-fontSizeEl.addEventListener('input', event => {
-  textEl.style.fontSize = event.currentTarget.value + 'px';
-});
