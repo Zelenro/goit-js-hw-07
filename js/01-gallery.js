44e7b310cb1696063285d4d4b99a83f2e1cf234a
Zelenro <125198729+Zelenro@users.noreply.github.com>
2023-06-23 08:37:54 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,31 +1,39 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 const galleryRef = document.querySelector('.gallery');
+
+const createGalleryItemsMarkup = ({ preview, original, description }) => {
+  return `
+  <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+  <img
+  class="gallery__image"
+  src="${preview}"
+  data-source="${original}"
+  alt="${description}"
+  />
+  </a>
+  </li>`;
+};
+
+const galleryItemsMarkup = galleryItems
+  .map(item => createGalleryItemsMarkup(item))
+  .join('');
+
+galleryRef.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
 
 galleryRef.addEventListener('click', handlerClickImg);
 
 function handlerClickImg(event) {
-  console.log(event.target);
+  event.preventDefault();
+  const target = event.target;
+  const imageSource = target.dataset.source;
+  // console.log('url великого зображення:', imageSource);
+
+  const instance = basicLightbox.create(imageSource);
+  console.log(instance.show());
+
+  if (!event.target.classList.contains('gallery__item')) {
+    return;
+  }
 }
-
-const createGalleryItemMarkup = ({ preview, original, description }) => {
-  return `
-    <li class="gallery__item">
-      <a class="gallery__link" href="${original}">
-        <img
-          class="gallery__image"
-          src="${preview}"
-        //   data-source="${original}"
-          alt="${description}"
-        />
-      </a>
-    </li>
-  `;
-};
-
-const galleryItemsMarkup = galleryItems
-  .map(item => createGalleryItemMarkup(item))
-  .join('');
-
-galleryRef.innerHTML = galleryItemsMarkup;
