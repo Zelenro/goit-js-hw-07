aa64c20d6b56df1b3057f53d3165c7d63001cdc4
Zelenro <125198729+Zelenro@users.noreply.github.com>
2023-06-23 14:56:50 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,5 @@
 import { galleryItems } from './gallery-items.js';
-
 const galleryRef = document.querySelector('.gallery');
-
 const createGalleryItemsMarkup = ({ preview, original, description }) => {
   return `
   <li class="gallery__item">
@@ -15,25 +13,66 @@ const createGalleryItemsMarkup = ({ preview, original, description }) => {
   </a>
   </li>`;
 };
-
 const galleryItemsMarkup = galleryItems
   .map(item => createGalleryItemsMarkup(item))
   .join('');
 
 galleryRef.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
-
 galleryRef.addEventListener('click', handlerClickImg);
 
+// window.addEventListener('keydown', keydownEscape);
+
+// function keydownEscape(event) {
+//   if (event.code === 'Escape') {
+//     instance.close();
+//   }
+// }
+
+// function handlerClickImg(event) {
+//   if (event.target === galleryRef) {
+//     return;
+//   }
+//   event.preventDefault();
+//   const target = event.target;
+//   const imageSource = target.dataset.source;
+//   const instance = basicLightbox.create(
+//     `<img src="${imageSource}" width="1400" height="900">`
+//     // {
+//     //   onShow: keydownEscape,
+//     //   onClose: keydownEscape,
+//     // }
+//   );
+//   instance.show();
+//   if (instance.show()) {
+//     function keydownEscape(event) {
+//       if (event.code === 'Escape') {
+//         instance.close();
+//       }
+//     }
+//   }
+// }
+
+// const modalOn = document.querySelector('.basicLightbox--visible');
+
+window.addEventListener('keydown', keydownEscape);
+
+function keydownEscape(event) {
+  const instance = basicLightbox.get();
+  if (event.code === 'Escape' && instance) {
+    instance.close();
+  }
+}
+
 function handlerClickImg(event) {
+  if (event.target === galleryRef) {
+    return;
+  }
   event.preventDefault();
   const target = event.target;
   const imageSource = target.dataset.source;
-  // console.log('url великого зображення:', imageSource);
-
-  const instance = basicLightbox.create(imageSource);
-  console.log(instance.show());
-
-  if (!event.target.classList.contains('gallery__item')) {
-    return;
-  }
+  const instance = basicLightbox.create(
+    `<img src="${imageSource}" width="1400" height="900">`
+  );
+  instance.show();
+  window.addEventListener('keydown', keydownEscape);
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+console.log(simpleLightbox());
+
+// const galleryRef = document.querySelector('.gallery');
+
+// const createGalleryItemsMarkup = ({ preview, original, description }) => {
+//   return `
+//     <li class="gallery__item">
+//       <a class="gallery__link" href="${original}">
+//         <img class="gallery__image" src="${preview}" alt="${description}" />
+//       </a>
+//     </li>
+//   `;
+// };
+
+// const galleryItemsMarkup = galleryItems
+//   .map(item => createGalleryItemsMarkup(item))
+//   .join('');
+
+// galleryRef.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
+
+// const lightbox = new SimpleLightbox('.gallery a');
+
+// function handlerClickImg(event) {
+//   if (event.target === galleryRef) {
+//     return;
+//   }
+//   event.preventDefault();
+//   const target = event.target;
+//   const imageSource = target.dataset.source;
+
+//   lightbox.open([{ src: imageSource }]);
+// }
+
+// galleryRef.addEventListener('click', handlerClickImg);
